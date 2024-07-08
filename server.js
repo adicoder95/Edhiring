0ce688fbcc4d6e59fb3a7f9a0fd2c87ef83a501d
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-07-08 12:43:26 +0530

    server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 
 const User = require('./models/user');
