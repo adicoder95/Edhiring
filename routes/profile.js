7b9990821a471873b487a2497455cbd8d7b066ff
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-07-09 14:40:44 +0530

    Add files via upload

---
+++ routes/profile.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { getProfile, updateProfile, updateDisplayPicture } = require("../controllers/profileController");
+const { auth } = require("../middlewares/auth");
+const upload = require("../middlewares/fileUpload");
+
+router.get("/", auth, getProfile);
+router.put("/", auth, updateProfile);
+router.put("/updateDisplayPicture", auth, upload.single('displayPicture'), updateDisplayPicture);
+
+module.exports = router;
