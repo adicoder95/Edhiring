e02f8e9cdb970b864531654acaaf0f88558ae46f
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-07-09 14:40:11 +0530

    Profile.js

--- models/Profile.js
+++ models/Profile.js
@@ -2,20 +2,33 @@ const mongoose = require("mongoose");
 
 // Define the Profile schema
 const profileSchema = new mongoose.Schema({
+	profilePic: {
+		type: String,
+		required: true,
+	},
 	gender: {
 		type: String,
-	},
-	dateOfBirth: {
-		type: String,
-	},
-	about: {
-		type: String,
-		trim: true,
+		required: true,
 	},
 	contactNumber: {
 		type: Number,
+		required: true,
 		trim: true,
 	},
+	currentCity:{
+		type: String,
+		required: true,
+	},
+	dateOfBirth: {
+		type: Date,
+		required: true,
+	},
+	about: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	
 });
 
 // Export the Profile model
