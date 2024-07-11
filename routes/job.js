df651f336a0436d3c42101eaff8385c36c8c05af
Stuti19 <63838650+Stuti19@users.noreply.github.com>
2024-07-11 13:47:52 +0530

    Update job.js

--- routes/job.js
+++ routes/job.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { createJob, singleJob, updateJob, showJobs } = require('../controllers/Job');
-const { isCandidate, isEmployer, isAdmin } = require('../middlewares/auth');
+const { protect,isCandidate, isEmployer, isAdmin } = require('../middlewares/auth');
 
 // Jobs routes
-router.post('/create', isAdmin, createJob);
+router.post('/create',protect, isAdmin, createJob);
 router.get('/job/:id', singleJob);
-router.put('/job/update/:job_id', isAdmin, updateJob);
+router.put('/job/update/:job_id',protect, isAdmin, updateJob);
 router.get('/jobs/show', showJobs);
 
 module.exports = router;
