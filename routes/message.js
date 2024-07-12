f1e7f95523b4078ccfcec21f2dd9838f10060aae
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-07-12 14:59:38 +0530

    Add files via upload

---
+++ routes/message.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const router = express.Router();
+const { sendMessage, getMessages, getSentMessages } = require('../controllers/message');
+const { protect } = require('../middlewares/auth');
+
+// Send a message to candidate
+router.post('/message/send', protect, sendMessage);
+
+// Get received messages 
+router.get('/message/received', getMessages);
+
+
+module.exports = router;
