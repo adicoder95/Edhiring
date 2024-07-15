2313ddc4fd25e96123fdc59acd33d0a358fe0153
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-07-15 18:38:08 +0530

    job.js

--- routes/job.js
+++ routes/job.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { createJob, singleJob, updateJob, showJobs } = require('../controllers/Job');
-const { protect,isCandidate, isEmployer, isAdmin } = require('../middlewares/auth');
+const { createJob, singleJob, updateJob, showJobs, getJobApplications, updateApplicationStatus } = require('../controllers/Job');
+const { isAuthenticated, protect, isCandidate, isEmployer, isAdmin } = require('../middlewares/auth');
 
 // Jobs routes
 router.post('/create',protect, isAdmin, createJob);
-router.get('/job/:id', singleJob);
-router.put('/job/update/:job_id',protect, isAdmin, updateJob);
+router.get('/job/:id', protect, isAdmin, singleJob);
+router.put('/job/update/:id',protect, isAdmin, updateJob);
 router.get('/jobs/show', showJobs);
+// // GET /api/v1/jobs/:jobId/applications
+// router.get('/:jobId/applications', protect, getJobApplications);
+
+// Route to get applications for jobs created by the logged-in employer
+router.post("/applications", isAuthenticated, isAdmin, getJobApplications);
+
+// Route to update the status of a job application
+router.put('/applications/:applicationId/status', protect, isAdmin, updateApplicationStatus);
+
 
 module.exports = router;
