106b7f005427267732bc6af808d40960408ccc59
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-07-16 22:04:11 +0530

    auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -67,6 +67,22 @@ exports.auth = async (req, res, next) => {
 		});
 	}
 };
+
+exports.isAuthenticated = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'No token provided' });
+    }
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+};
+
 exports.isCandidate = async (req, res, next) => {
 	try {
 		const userDetails = await User.findOne({ email: req.user.email });
