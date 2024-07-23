feea3adff2a480b954e1577a36b348f999fa5eda
Stuti19 <63838650+Stuti19@users.noreply.github.com>
2024-07-23 18:34:52 +0530

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,25 +1,15 @@
-const cloudinary = require('cloudinary').v2;
-require('dotenv').config(); // Make sure to have dotenv to load environment variables
+const cloudinary = require('cloudinary').v2
 
-// Configure Cloudinary with your credentials
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
+//for uploading image
+exports.uploadImageToCloudinary  = async (file, folder, height, quality) => {
+    const options = {folder};
+    if(height) {
+        options.height = height;
+    }
+    if(quality) {
+        options.quality = quality;
+    }
+    options.resource_type = "auto";
 
-const uploadImageToCloudinary = async (filePath, folderName, width, height) => {
-  try {
-    const result = await cloudinary.uploader.upload(filePath, {
-      folder: folderName,
-      width: width,
-      height: height,
-      crop: 'limit'
-    });
-    return result;
-  } catch (error) {
-    throw new Error('Failed to upload image to Cloudinary');
-  }
-};
-
-module.exports = { uploadImageToCloudinary };
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
