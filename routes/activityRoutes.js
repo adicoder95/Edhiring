cd9a0350c4b3777eb42fccf68cbb526f39240c66
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-07-26 21:08:53 +0530

    activityRoutes.js

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getRecentActivities, toggleStarActivity } = require('../controllers/activityController');
+const { getRecentActivities, toggleStarActivity, getStarredActivities } = require('../controllers/activityController');
 const { isAuthenticated } = require('../middlewares/auth');
 
 router.get('/activities', isAuthenticated, getRecentActivities);
 router.put('/activities/:id/star', isAuthenticated, toggleStarActivity);
+router.get('/activities/starred', isAuthenticated, getStarredActivities);
 
 module.exports = router;
