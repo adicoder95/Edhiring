efa929b8bdc997a28119591948edf8830b6f0f84
Stuti19 <63838650+Stuti19@users.noreply.github.com>
2024-07-29 20:09:49 +0530

    Update and rename message.js to room.js

---
+++ models/room.js
@@ -0,0 +1,23 @@
+// models/Room.js
+
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    sender: String,
+    message: String,
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const roomSchema = new mongoose.Schema({
+    roomId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    messages: [messageSchema],
+});
+
+module.exports = mongoose.model('Room', roomSchema);
