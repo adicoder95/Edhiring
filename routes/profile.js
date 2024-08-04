d19ee3e0843c0b4400ce4ab7703a0b5b69072ddd
Stuti19 <63838650+Stuti19@users.noreply.github.com>
2024-08-04 22:43:58 +0530

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -2,11 +2,11 @@
 const express = require("express");
 const router = express.Router();
 const { getProfile, updateProfile, updateProfilePic } = require("../controllers/profileController");
-const { auth } = require("../middlewares/auth");
+const { auth,isCandidate } = require("../middlewares/auth");
 const upload = require("../middlewares/fileUpload");
 
-router.get("/", auth, getProfile);
-router.put("/", auth, updateProfile);
-router.put("/updateProfilePic", auth, updateProfilePic);
+router.get("/", auth,isCandidate, getProfile);
+router.put("/", auth,isCandidate, updateProfile);
+router.put("/updateProfilePic", auth,isCandidate, updateProfilePic);
 
 module.exports = router;
