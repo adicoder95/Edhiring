e3a95bf4ad8198596f96a95728508d5d12b98aa2
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-08-13 21:06:07 +0530

    profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -7,6 +7,5 @@ const upload = require("../middlewares/fileUpload");
 
 router.get("/", auth,isCandidate, getProfile);
 router.put("/", auth,isCandidate, updateProfile);
-router.put("/updateProfilePic", auth,isCandidate, updateProfilePic);
 
 module.exports = router;
