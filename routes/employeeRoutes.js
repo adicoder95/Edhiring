48a87289d5679cc0d3b7795faae90914dc9b1e76
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-08-13 22:45:52 +0530

    employeeRoutes.js

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { getEmployerProfile, updateEmployerProfile, updateEmployerLogo, updateEmployerCoverPic } = require('../controllers/employeeController');
+const { getEmployerProfile, updateEmployerProfile, getInstituteInfo } = require('../controllers/employeeController');
 const { isAuthenticated, isEmployer, auth } = require('../middlewares/auth');
 
 // Route to get the employer profile
@@ -9,5 +9,8 @@ router.get('/profile', isAuthenticated, isEmployer, getEmployerProfile);
 
 // Route to update the employer profile
 router.put('/profile/update', auth, isEmployer, updateEmployerProfile);
+
+router.get('/profile/update/inst',auth, isEmployer, getInstituteInfo);
+
 
 module.exports = router;
