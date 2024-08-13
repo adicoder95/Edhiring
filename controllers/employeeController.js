6d14582309afcc34463d600060fc6f3e31c823f7
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-08-13 21:05:09 +0530

    employeeController.js

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -33,136 +33,87 @@ exports.getEmployerProfile = async (req, res) => {
 
 
 exports.updateEmployerProfile = async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).populate('additionalDetails');
+  try {
+      const user = await User.findById(req.user.id).populate('additionalDetails');
 
-        console.log("user values"+user);
-        if (!user ) {
-            return res.status(404).json({ success: false, message: 'Employer profile not found' });
-        }
+      console.log("user values"+user);
+      if (!user ) {
+          return res.status(404).json({ success: false, message: 'Employer profile not found' });
+      }
 
 
-        const employerProfileId = user.additionalDetails;
-        let { logo, coverPhoto, email, contact, instituteName, institueContact, instituteEmail, website, foundingDate, socialNetwork, about, currentCity, pincode, address1, address2, address3, fullName } = req.body;
+      const employerProfileId = user.additionalDetails;
+      let logoPic = req.files.logo;
+      let coverPic = req.files.coverPhoto;
+      let { email, contact, instituteName, institueContact, instituteEmail, website, foundingDate, socialNetwork, about, currentCity, pincode, address1, address2, address3, fullName } = req.body;
 
-        // Create an update object
-        const updateData = {
-            logo,
-            coverPhoto,
-            email,
-            contact,
-            instituteName,
-            institueContact,
-            instituteEmail,
-            website,
-            foundingDate,
-            socialNetwork,
-            about,
-            pincode,
-            address1,
-            address2,
-            address3,
-            currentCity,
-            fullName,
-        };
+      const image1 = await uploadImageToCloudinary(
+        logoPic,
+        process.env.FOLDER_NAME,
+        1000,
+        1000
+      )
+      const image2 = await uploadImageToCloudinary(
+        coverPic,
+        process.env.FOLDER_NAME,
+        1000,
+        1000
+      )
 
-        // Remove undefined fields from the update object
-        Object.keys(updateData).forEach(key => {
-            if (updateData[key] === undefined) {
-                delete updateData[key];
-            }
-        });
-        console.log("updated data:"+updateData);
-        const employer = await Employer.findByIdAndUpdate(
-            employerProfileId,
-            updateData,
-            { new: true, runValidators: true }
-        );
-        console.log(employerProfileId);
+      // Create an update object
+      const updateData = {
+          email,
+          contact,
+          instituteName,
+          institueContact,
+          instituteEmail,
+          website,
+          foundingDate,
+          socialNetwork,
+          about,
+          pincode,
+          address1,
+          address2,
+          address3,
+          currentCity,
+          fullName,
+      };
 
-        await recordActivity(req.user.id, 'Profile', `Updated Profile`); //recent activity
+      const updatedProfile1 = await Employer.findByIdAndUpdate(
+        user.additionalDetails._id,
+        { logo : image1.secure_url },
+        { new: true }
+      );
 
-        res.status(200).json({ success: true, employer });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, message: 'Server error' });
-    }
+      const updatedProfile2 = await Employer.findByIdAndUpdate(
+        user.additionalDetails._id,
+        { coverPhoto : image2.secure_url },
+        { new: true }
+      );
+
+      // Remove undefined fields from the update object
+      Object.keys(updateData).forEach(key => {
+          if (updateData[key] === undefined) {
+              delete updateData[key];
+          }
+      });
+      console.log("updated data:"+updateData);
+      const employer = await Employer.findByIdAndUpdate(
+          employerProfileId,
+          updateData,
+          updatedProfile1,
+          updatedProfile2,
+          { new: true, runValidators: true }
+      );
+      console.log(employerProfileId);
+
+      await recordActivity(req.user.id, 'Profile', `Updated Profile`); //recent activity
+
+      res.status(200).json({ success: true, employer });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: 'Server error' });
+  }
 };
 
 
-
-exports.updateEmployerLogo = async (req, res) => {
-    try {
-      const profilePic = req.files.logoPic
-      const userId = req.user.id
-      
-
-      const image = await uploadImageToCloudinary(
-        profilePic,
-        process.env.FOLDER_NAME,
-        1000,
-        1000
-      )
-      console.log(image.secure_url)
-
-      const user = await User.findById(userId).populate('additionalDetails');
-
-      const updatedProfile = await Employer.findByIdAndUpdate(
-        user.additionalDetails._id,
-        { logo : image.secure_url },
-        { new: true }
-      );
-
-      await recordActivity(req.user.id, 'Profile', `Updated Logo`); //recent activity
-
-      res.send({
-        success: true,
-        message: `Image Updated successfully`,
-        data: updatedProfile,
-      });
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        message: error.message,
-      });
-    }
-  };
-
-
-
-exports.updateEmployerCoverPic = async (req, res) => {
-    try {
-      const profilePic = req.files.coverPic
-      const userId = req.user.id
-      
-
-      const image = await uploadImageToCloudinary(
-        profilePic,
-        process.env.FOLDER_NAME,
-        1000,
-        1000
-      )
-      console.log(image.secure_url)
-
-      const user = await User.findById(userId).populate('additionalDetails');
-
-      const updatedProfile = await Employer.findByIdAndUpdate(
-        user.additionalDetails._id,
-        { coverPhoto : image.secure_url },
-        { new: true }
-      );
-
-      await recordActivity(req.user.id, 'Profile', `Updated cover picture`); //recent activity
-
-      res.send({
-        success: true,
-        message: `Image Updated successfully`,
-        data: updatedProfile,
-      });
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        message: error.message,
-      });
-    }
-  };
