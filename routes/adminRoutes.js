e824e0a0c79562c408ce15ca26fbe95f0f1554f8
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-08-20 18:26:04 +0530

    adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getEmployersData, getCandidatesData } = require('../controllers/adminController');
-const { isAdmin, auth } = require('../middlewares/auth');
+const { isAdmin, auth, isAuthenticated } = require('../middlewares/auth');
 
 
 // Route to get employers' data
-router.get('/employers',auth, isAdmin, getEmployersData);
+router.get('/employers',auth, isAuthenticated, isAdmin, getEmployersData);
 
-router.get('/candidates',auth, isAdmin, getCandidatesData);
+router.get('/candidates',auth, isAuthenticated, isAdmin, getCandidatesData);
 module.exports = router;
