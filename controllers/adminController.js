cc9e5de46bad8653e41fa0d6c8be5a94f2100092
Muskan Garg <124268826+MUSKAN-GARG1108@users.noreply.github.com>
2024-08-20 18:07:35 +0530

    Add files via upload

---
+++ controllers/adminController.js
@@ -0,0 +1,41 @@
+// controllers/adminController.js
+const Employers = require('../models/employerProfile'); 
+const User = require('../models/User'); 
+const Candidate = require('../models/Profile');
+
+
+exports.getEmployersData = async (req, res) => {
+  try {
+    // Fetch all employers and their details
+    const employers = await User.find({ accountType: 'Employer' }).populate('additionalDetails');
+
+    // Return the count and details of employers
+    res.status(200).json({
+      success: true,
+      count: employers.length,
+      employers,
+    });
+  } catch (error) {
+    console.error('Error fetching employers data:', error);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+};
+
+
+exports.getCandidatesData = async (req, res) => {
+  try {
+    // Fetch all candidates and their details
+    const candidates = await User.find({ accountType: 'Candidate' }).populate('additionalDetails');
+
+    // Return the count and details of candidates
+    res.status(200).json({
+      success: true,
+      count: candidates.length,
+      candidates,
+    });
+  } catch (error) {
+    console.error('Error fetching candidates data:', error);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+};
+
